1a64a0c4b5253d978a568db6837acbc316125df5
bpthomasyeow <156176328+bpthomasyeow@users.noreply.github.com>
2024-01-12 11:58:35 +0100

    Add files via upload

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,6 +1,7 @@
 #Pokażę parę fajnych rzeczy
 import streamlit as st
 import numpy as np
+import base64
 
 #Moduł streamlit całkowicie eliminuje konieczność używania
 #HTML/JS/CSS w sytuacjach, gdzie nie musimy spersonalizować
@@ -16,7 +17,7 @@ st.markdown("---")
 liczba = 777
 #a tu wysyłamy do strony
 liczba
-#jak dbamy o wygląd jakiegoś elementu,
+#jak dbamy o wyglądu jakiegoś elementu,
 #zawsze możemy wcisnąć HTML/CSSa
 st.markdown(f"<h2 style='text-align: center;'>{liczba}</h2>",
 unsafe_allow_html = True)
@@ -25,9 +26,16 @@ tabela = np.random.randn(5,10)
 #tą linią pokażesz tabelę
 st.dataframe(tabela)
 
-#prosty user input, zmienia wartość zmiennej 'liczba' powyżej
-
+#prosty user input, zmienia wartość zmiennej 'wartosc' poniżej
 wartosc = st.text_input("User input: ", "boop")
-
 st.write(wartosc)
 
+#download button - dodaje _ po każdej literce w wartosc
+def downloadtxt():
+    txt_for_download = "_".join(list(wartosc))
+    b64 = base64.b64encode(txt_for_download.encode()).decode()
+    href = f'<a href="data:file/txt;base64,{b64}" download="sample.txt">Download Text File</a>'
+    st.markdown(href, unsafe_allow_html=True)
+    
+if st.button("Pobierz"):
+    downloadtxt()
